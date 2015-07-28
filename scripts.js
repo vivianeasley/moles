8c42b8bd3ae0b01e4c1f61923d91a372fa095d51
Robert Easley <destroyallculture@gmail.com>
2015-07-28 08:49:12 -0700

    Added game back.

--- scripts.js
+++ scripts.js
@@ -9,14 +9,14 @@ var VIEWS = (function ( ) {
   VE.buildIntroView = function ( ) {
     var introScreenData = "<div class='pop-up-wrapper'>\
         <div class='pop-up'>\
-          <div class='pop-up-title'>Welcome to Whack-A-Mole</div>\
+          <div class='pop-up-title'>Welcome to Molee McMolerson's Molatorium</div>\
           <div class='instructions-wrapper'>\
             <div>It’s time to protect your vegetables! Whack each mole you find in your garden to score one point! If you miss and it escapes you loose a vegetable. There will be five offensives from the mole army, each with faster moles and more holes. Don’t loose all your vegetables - you have to save your harvest!</div>\
             <div class='instructions-chunk'>\
               <img class='instructions-image' src='assets/mole.png'><span>Your average, everyday mole. Whack it to receive one point.</span>\
             </div>\
             <div class='instructions-chunk'>\
-              <img class='instructions-image' src='assets/queen-mole.png'><span>A rare red queen mole. Whack it to get 5 points and an extra vegetable.</span>\
+              <img class='instructions-image' src='assets/queen-mole.png'><span>A rare blue queen mole. Whack it to get 5 points and an extra vegetable.</span>\
             </div>\
             <div class='instructions-chunk'>\
               <img class='instructions-image' src='assets/double-mole.png'><span>Whack two moles at the same time and a hole will collapse. You also score two points.</span>\
@@ -95,7 +95,14 @@ var VIEWS = (function ( ) {
 
   };
 
+  VE.buildMoleView = function ( ) {
+     var levelScreenData = "<div class='mole-wrapper'>\
+            <img class='mole-sprite' src='assets/mole-sprite.png'>\
+          </div>";
 
+    return buildViewNode( levelScreenData );
+
+  };
 
 
   // Private Functions
@@ -484,6 +491,197 @@ var LIFE = (function ( ) {
   return LI;
 }( ));
 
+var MUSIC = (function ( ) {
+
+  // Public Variables
+  var MU = { };
+
+  // Private Variables
+  var fLoseNotes;
+  var fLevelNotes;
+  var fWhen;
+  var fTempo = 72;
+
+  var fSequence1;
+  var fSequence2;
+  var fSequence3;
+
+  var fSequence4;
+  var fSequence5;
+  var fSequence6;
+
+  var fSounds = typeof AudioContext !== 'undefined' ? new AudioContext : new webkitAudioContext;
+    
+  // Public Functions
+  MU.init = function( ) {
+    buildMusic( );
+    buildLoseSound( );
+    buildLeveledSound( );
+  }
+
+  MU.playMusic = function ( ) {
+    fWhen = fSounds.currentTime;
+    fSequence1.play( fWhen );
+    fSequence2.play( fWhen );
+    fSequence3.play( fWhen );
+  }
+
+  MU.playLoseSound = function ( ) {
+    fSequence4.play( );
+  }
+
+  MU.playLeveledSound = function ( ) {
+    fSequence5.play( );
+  }
+
+  MU.stopMusic = function ( ) {
+    fSequence1.stop( );
+    fSequence2.stop( );
+    fSequence3.stop( );
+  }
+
+  MU.stopLoseSound = function ( ) {
+    fSequence4.stop( );
+  }
+
+
+  MU.incrementTempo = function ( ) {
+    fTempo = fTempo + 17;
+    MU.init( );
+  }
+
+  MU.resetTempo = function ( ) {
+    fTempo = 72;
+    MU.init( );
+  }
+
+  // Private Functions
+
+  function buildMusic ( ) {
+    fWhen = fSounds.currentTime,
+
+      lead = [
+        'Bb2  e',
+        'C3   e',
+        'B2   s',
+        'Bb2  e',
+        'F2   q',
+        'A1   e',
+        'C3   e',
+        'A2   s',
+        'Bb2  s',
+        'C2   q',
+        'Bb3  e',
+        'C4   e',
+        'B3   s',
+        'Bb3  e',
+        'C3   q',
+        'A2   e',
+        'C4   e',
+        'A3   s',
+        'Bb3  s',
+        'C3   q',
+      ],
+      harmony = [
+        'Aa1  e',
+        '-    e',
+        '-    s',
+        'Bb1  e',
+        'F2   q',
+        'C4   e',
+        'A4   e',
+        'C4   s',
+        'Bb4  s',
+        'C3   q',
+        'Aa2  e',
+        '-    e',
+        '-    s',
+        'Bb2  e',
+        'F3   q',
+        'C5   e',
+        'A5   e',
+        'C5   s',
+        'Bb5  s',
+        'C4   q',
+      ],
+      bass = [
+        '-    e',
+        '-    e',
+        '-    s',
+        '-    e',
+        'A2   q',
+        '-    e',
+        '-    e',
+        '-    s',
+        '-    s',
+        'A2   q',
+      ];
+
+    fSequence1 = new Sequence( fSounds, fTempo, lead );
+    fSequence2 = new Sequence( fSounds, fTempo, harmony );
+    fSequence3 = new Sequence( fSounds, fTempo, bass );
+
+    fSequence1.staccato = 0.55;
+    fSequence2.staccato = 0.55;
+    fSequence3.staccato = 0.05;
+    fSequence3.smoothing = 0.4;
+
+    fSequence1.gain.gain.value = 1.0 / 90;
+    fSequence2.gain.gain.value = 0.8 / 90;
+    fSequence3.gain.gain.value = 0.65 / 90;
+
+    fSequence1.mid.frequency.value = 800;
+    fSequence1.mid.gain.value = 3;
+    fSequence2.mid.frequency.value = 1200;
+    fSequence3.mid.gain.value = 3;
+    fSequence3.bass.gain.value = 6;
+    fSequence3.bass.frequency.value = 80;
+    fSequence3.mid.gain.value = -6;
+    fSequence3.mid.frequency.value = 500;
+    fSequence3.treble.gain.value = -2;
+    fSequence3.treble.frequency.value = 1400;
+  }
+
+  function buildLoseSound ( ) {
+    loseSoundTempo = 72;
+
+    fLoseNotes = [
+      'A2  q',
+      'E2  q',
+      'E1  h',
+    ];
+
+    fSequence4 = new Sequence( fSounds, loseSoundTempo, fLoseNotes );
+    fSequence4.gain.gain.value = 0.65 / 90;
+    fSequence4.loop = false;
+  }
+
+  function buildLeveledSound ( ) {
+    levelSoundTempo = 120;
+
+    fLevelNotes = [
+      'C5  e',
+      'F4  e',
+      'A4  e',
+      'C5  e',
+      'F5  e',
+      'A5  e',
+      'C6  e',
+      'F6  q',
+    ];
+
+    fSequence5 = new Sequence( fSounds, levelSoundTempo, fLevelNotes );
+    fSequence5.gain.gain.value = 0.65 / 90;
+    fSequence5.loop = false;
+  }
+
+
+
+  return MU;
+}( ));
+
+
+
 var MAIN = (function ( ) {
 
   // Public Variables
@@ -506,15 +704,21 @@ var MAIN = (function ( ) {
     
 
   MA.init = function ( ) {
+    //Add fast click to the body. This is supposed to remove the 300ms delay on iOS devices.
+    var attachFastClick = Origami.fastclick;
+    attachFastClick(document.body);
+
     POPUP.installPopUp( 'intro', MAIN.restartGame );
     
   };
 
   /////////////////// Main Game Logic and Mole Handling ////////////////////////////
-
-  MA.gameLoop = function ( ) {
+  // The main game loops happens in 3 steps, with 3 main functions.
+  MA.startGameLoop = function ( ) {
     // If the game has ended makes game loop stop.
     if ( MA.gameOver === true ) {
+      MUSIC.stopMusic( );
+      MUSIC.playLoseSound( );
       MA.gameOver = false;
       return;
     }
@@ -522,11 +726,16 @@ var MAIN = (function ( ) {
     // This checks you score and increases the level, or registers a win, accordingly.
     if ( MA.score > fLevelIncrement ) {
       if ( MA.currentLevel > 3 ) {
+        MUSIC.stopMusic( );
+        MUSIC.playLeveledSound( );
         POPUP.installPopUp( 'win', MAIN.restartGame )
         return;
       } 
       fLevelIncrement += 15;
       increaseLevelAttributes( );
+      MUSIC.stopMusic( );
+      MUSIC.playLeveledSound( );
+      MUSIC.incrementTempo( );
       POPUP.installPopUp( 'leveled', MAIN.continueGame );
       MA.currentLevel++;
       return;
@@ -535,11 +744,12 @@ var MAIN = (function ( ) {
     // Get the amount of time to wait until a mole appears.
     var randomTimeInterval = GENERAL.getRandomNum(700, 1300); 
 
-    GENERAL.wait( randomTimeInterval, chooseMoleType )
+    GENERAL.wait( randomTimeInterval, buildPlaceMole )
   }
 
-  function chooseMoleType ( ) {
+  function buildPlaceMole ( ) {
     // This randomly decides if you will get a double, single, or queen mole.
+    // then calls create mole to build and append mole.
     var randNum = GENERAL.getRandomNum(0, 100);
 
     if ( randNum < 80 ) {
@@ -555,10 +765,10 @@ var MAIN = (function ( ) {
 
     }
 
-    GENERAL.wait( fMoleSpeedMilSec, removeMole );
+    GENERAL.wait( fMoleSpeedMilSec, removeUncaughtMoles );
   }
 
-  function removeMole ( ) { 
+  function removeUncaughtMoles ( ) { 
     //Handles Removing of uncaught moles and then continues the loop
     var moles = document.querySelectorAll('.mole-wrapper');
 
@@ -580,12 +790,14 @@ var MAIN = (function ( ) {
 
     fDoubleMole = false;
 
-    MA.gameLoop( ); 
+    MA.startGameLoop( ); 
   }
 
   /////////////////// Create Mole and Randomly Place It ////////////////////////////
 
   function createMole ( moleType ) { 
+    var moleNode;
+    var imgSpriteNode;
     var randomHole  = GENERAL.getRandomNum( 0, (fHoleNodes.length - 1) );
     var imagePath   = 'assets/' + moleType + '-sprite.png';
     var boxOccupied = fHoleNodes[randomHole].querySelector('.mole-sprite'); 
@@ -600,17 +812,18 @@ var MAIN = (function ( ) {
     }
 
     // Build mole and append. Add click events for mole.
-    var moleWrapper = document.createElement('div');
-    moleWrapper.classList.add('mole-wrapper');
+    moleNode      = VIEWS.buildMoleView( );
+    imgSpriteNode = moleNode.querySelector('.mole-sprite');
 
-    var newNode = NODES.buildReturnNode( 'img', imagePath, 'mole-sprite', moleWrapper );
-    moleWrapper.appendChild( newNode );
+    if ( moleType === 'queen-mole' ) {
+      imgSpriteNode.src = imagePath;
+    }
 
-    fHoleNodes[randomHole].appendChild( moleWrapper );
+    fHoleNodes[randomHole].appendChild( moleNode );
 
-    SPRITE.animate( newNode, 116, 103, 50, 12, undefined );
+    SPRITE.animate( imgSpriteNode, 116, 103, 50, 12, undefined );
 
-    moleWrapper.addEventListener("mousedown", function ( ) { moleCaptured( this, moleType ); }, false)
+    moleNode.addEventListener("mousedown", function ( ) { moleCaptured( this, moleType ); }, false)
 
   }
 
@@ -631,6 +844,8 @@ var MAIN = (function ( ) {
     MA.gameOver         = false;
 
     fScoreNode.textContent = 0;
+
+    MUSIC.resetTempo( );
     
     var lifeVeggy = document.querySelectorAll('.life');
 
@@ -656,9 +871,11 @@ var MAIN = (function ( ) {
   }
 
   MA.continueGame = function ( ) {
+    MUSIC.stopLoseSound( );
+    MUSIC.playMusic( );
     var popUpNode = document.querySelector('.pop-up-wrapper')
     document.body.removeChild(popUpNode);
-    MA.gameLoop( );
+    MA.startGameLoop( );
   }
 
 
